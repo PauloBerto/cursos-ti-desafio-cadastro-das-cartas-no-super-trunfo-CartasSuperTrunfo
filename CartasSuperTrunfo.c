cf57f1f95faec73c5344bbeb6046a7ce9f1fecc0
Paulo Henrique Berto Silva <61211755+PauloBerto@users.noreply.github.com>
2026-03-17 02:13:01 +0000

    Realizado segunda parte do desafio novato implementando as comparações de atribuos

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#include <stdio.h>
+
 int main() {
 
 
@@ -11,7 +13,9 @@ int main() {
     int pontos[2];
     float area[2];
     float PIB[2];
-    
+    float densidade[2];
+    float pibpercapita[2];
+
     // ---------------------------
     // Cadastro da Carta 1
     // ---------------------------
@@ -64,12 +68,81 @@ int main() {
     // Exibicao das Cartas
     // ---------------------------
     printf("\n--- Carta 1 ---\n");
-    printf("Estado: %c\nCodigo: %s\nCidade: %s\nPopulacao: %d\nPontos turisticos: %d\nArea: %.2f km2\nPIB: %.2f bilhoes\n", 
+    printf("Estado: %c\nCodigo: %s\nCidade: %s\nPopulacao: %d\nPontos turísticos: %d\nArea: %.2f km2\nPIB: %.2f bilhoes\n", 
     estado[0], codigo[0], cidade[0], populacao[0], pontos[0], area[0], PIB[0]);
 
     printf("\n--- Carta 2 ---\n");
-    printf("Estado: %c\nCodigo: %s\nCidade: %s\nPopulacao: %d\nPontos turisticos: %d\nArea: %.2f km2\nPIB: %.2f bilhoes\n", 
+    printf("Estado: %c\nCodigo: %s\nCidade: %s\nPopulacao: %d\nPontos turísticos: %d\nArea: %.2f km2\nPIB: %.2f bilhoes\n", 
     estado[1], codigo[1], cidade[1], populacao[1], pontos[1], area[1], PIB[1]);
+
+
+    // ---------------------------
+    // Calculo densidade e PIB per capita
+    // ---------------------------
+    densidade[0] = populacao[0] / area[0];
+    densidade[1] = populacao[1] / area[1];
+
+    pibpercapita[0] = (PIB[0] * 1000000000) / populacao[0];
+    pibpercapita[1] = (PIB[1] * 1000000000) / populacao[1];
+
+    printf("\nDensidade da carta 1: %.2f\n", densidade[0]);
+    printf("PIB per capita da carta 1: %.2f\n", pibpercapita[0]);
+    printf("Densidade da carta 2: %.2f\n", densidade[1]);
+    printf("PIB per capita da carta 2: %.2f\n", pibpercapita[1]);
+
+    // ---------------------------
+    // Comparando atributos
+    // ---------------------------
+
+    printf("\nComparando os atributos de cada carta:\n");
+    if (populacao[0] > populacao[1]){
+        printf("\nA população da carta 1 é maior.\n");
+        printf("Carta 1 venceu!\n");
+    } else { 
+        printf("\nA população da carta 2 é maior.\n");
+        printf("Carta 2 venceu!\n");
+    }
+
+    if (pontos[0] > pontos[1]){
+        printf("\nA carta 1 possui mais pontos turísticos.\n");
+        printf("Carta 1 venceu!\n");
+    } else { 
+        printf("\nA carta 2 possui mais pontos turísticos.\n");
+        printf("Carta 2 venceu!\n");
+    }
+
+    if (area[0] > area[1]){
+        printf("\nA área da carta 1 é maior.\n");
+        printf("Carta 1 venceu!\n");
+    } else { 
+        printf("A área da carta 2 é maior.\n");
+        printf("Carta 2 venceu!\n");
+    }
+
+    if (PIB[0] > PIB[1]){
+        printf("\nO PIB da carta 1 é maior.\n");
+        printf("Carta 1 venceu!\n");
+    } else { 
+        printf("\nO PIB da carta 2 é maior.\n");
+        printf("Carta 2 venceu!\n");
+    }
+
+    if (densidade[0] > densidade[1]){
+        printf("\nA densisade da carta 1 é maior.\n");
+        printf("Carta 1 venceu!\n");
+    } else { 
+        printf("\nA densidade da carta 2 é maior.\n");
+        printf("Carta 2 venceu!\n");
+    }
+
+    if (pibpercapita[0] > pibpercapita[1]){
+        printf("\nO PIB per capita da carta 1 é maior.\n");
+        printf("Carta 1 venceu!\n");
+    } else { 
+        printf("\nO PIB per capita da carta 2 é maior.\n");
+        printf("Carta 2 venceu!\n");
+    }
 
     return 0;
 }
+
